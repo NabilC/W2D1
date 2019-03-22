7d2b6b35572582cfeb1f7a4183b60659a1bc9479
NabilC <NabilC@users.noreply.github.com>
2019-03-21 18:57:08 -0700

    Add files via upload
    SImon game

---
+++ Simon/lib/simon.rb
@@ -0,0 +1,53 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @seq = []
+    @sequence_length = 1
+    @game_over = false
+
+  end
+
+  def play
+    until self.game_over == true
+      self.take_turn
+    end
+    self.game_over_message
+    self.reset_game
+  end
+
+  def take_turn
+    self.show_sequence
+    self.require_sequence
+    self.round_success_message if !game_over 
+    @sequence_length += 1
+  end
+
+  def show_sequence
+    self.add_random_color
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+  end
+
+  def round_success_message
+
+  end
+
+  def game_over_message
+
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
